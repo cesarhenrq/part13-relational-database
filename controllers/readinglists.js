1d57c9b07f0ab00d4f937ca61e6e97511fc21b39
Cesar Henrique <cesaarhvb@gmail.com>
2025-05-11 02:06:40 -0300

    Exercise 13.22.

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -21,4 +21,19 @@ router.post("/", tokenExtractor, async (req, res) => {
   res.json(readingList);
 });
 
+router.put("/:id", tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id);
+  if (!readingList) {
+    return res.status(404).json({ error: "Reading list not found" });
+  }
+
+  if (readingList.userId !== req.decodedToken.id) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  readingList.read = req.body.read;
+  await readingList.save();
+  res.json(readingList);
+});
+
 module.exports = router;
